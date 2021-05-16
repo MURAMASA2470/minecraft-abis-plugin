ac1862961795cad640af6031cf65d3238ac66669
MURAMASA2470 <keisuke226214@yahoo.co.jp>
2021-05-16 23:49:07 +0900

    コマンド実行時のロジック追加

--- src/main/java/app/muramasa/minecraftplugin/MinecraftPlugin.kt
+++ src/main/java/app/muramasa/minecraftplugin/MinecraftPlugin.kt
@@ -16,6 +16,16 @@ class MinecraftPlugin : JavaPlugin() {
         logger.info("Hello!!")
     }
 
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+
+        when (sender) {
+            is Player -> {
+                logger.info(String.format("%s : %s : %s", command.name, label, sender.location.blockY))
+            }
+        }
+        return true
+    }
+
     override fun onDisable() {
         // Plugin shutdown logic
         HandlerList.unregisterAll(this)
