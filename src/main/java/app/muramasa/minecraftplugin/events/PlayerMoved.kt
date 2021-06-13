bc0a9f002292bd5c28fbb44256c0a2de67b1dedc
MURAMASA2470 <keisuke226214@yahoo.co.jp>
2021-06-13 15:45:45 +0900

    付与するエフェクト一覧作成

--- src/main/java/app/muramasa/minecraftplugin/events/PlayerMoved.kt
+++ src/main/java/app/muramasa/minecraftplugin/events/PlayerMoved.kt
@@ -13,24 +13,41 @@ import org.bukkit.potion.PotionEffectType
 
 class PlayerMoved : Listener {
 
+    private var effects : Map<Int, Collection<PotionEffect>> = mutableMapOf()
+
     constructor(plugin: Plugin) {
         plugin.server.pluginManager.registerEvents(this, plugin)
+
+        this.effects = mutableMapOf(
+            60 to mutableListOf(
+                PotionEffect(PotionEffectType.HUNGER, 60, 60),
+                PotionEffect(PotionEffectType.SLOW_DIGGING, 60, 60)
+            ),
+            50 to mutableListOf(
+                PotionEffect(PotionEffectType.SLOW, 60, 60),
+                PotionEffect(PotionEffectType.UNLUCK, 60, 60),
+                PotionEffect(PotionEffectType.HUNGER, 60, 60)
+            ),
+            40 to mutableListOf(
+                PotionEffect(PotionEffectType.WITHER, 60, 60),
+                PotionEffect(PotionEffectType.POISON, 60, 60)
+            )
+        )
     }
 
-    @EventHandler(priority = EventPriority.HIGHEST)
+    @EventHandler(priority = EventPriority.NORMAL)
     fun onMove(e: PlayerMoveEvent) {
 
+
         var player : Player = e.player
-        var from : Location = e.from
-        var to : Location? = e.to
+        var loc : Location = player.getLocation()
 
-//        player.sendMessage(String.format("%s -> x: %s, y: %s, z: %s", e.eventName, from.x, from.y, from.z))
-
-        if (from.y < 60) {
+        if (loc.y < 60) {
             player.sendTitle("y=60以下", "", 1, 3, 1)
 
-            var poisonEffect = PotionEffect(PotionEffectType.POISON, 60, 60)
-            player.addPotionEffect(poisonEffect)
+            player.addPotionEffect(PotionEffect(PotionEffectType.POISON, 60, 60))
+
+
         } else {
             player.removePotionEffect(PotionEffectType.POISON)
         }
